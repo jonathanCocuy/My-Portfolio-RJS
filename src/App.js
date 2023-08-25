2150e4b5d0f1464db871c3e6e768f30be0ad61b1
jonathanCocuy <jonathancp145@gmail.com>
2023-08-24 23:43:00 -0500

    First step

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/about">Acerca de mí</Link>
+            </li>
+            <li>
+              <Link to="/projects">Proyectos</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contacto</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
+      <Routes>
+        <Route path="/about">
+          <About/>
+        </Route>
+        <Route path="/projects">
+          <Projects/>
+        </Route>
+        <Route path="/contact">
+          <Contact/>
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function About() {
+    return <div>Acerca de mí</div>
+    
+}
+
+export default About;
