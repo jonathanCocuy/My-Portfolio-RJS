bee838c5086090e97c1208352206355e45223581
jonathanCocuy <jonathancp145@gmail.com>
2023-08-28 20:52:37 -0500

    Thirty-Step

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import About from "./Components/Main/About/About";
 import Projects from "./Components/Main/Projects/Projects";
+import Home from "./Components/Main/Home/Home";
+import Contact from "./Components/Main/Contact";
 import "./App.css";
 
 /* Barra de navegación */
@@ -9,6 +11,9 @@ function App() {
     <Router>
       <div className="div-nav">
         <ul className="navegation">
+          <li className="list-nav">
+            <Link to="/home" className="items-nav">Home</Link>
+          </li>
           <li className="list-nav">
             <Link to="/about" className="items-nav">Acerca de mí</Link>
           </li>
@@ -21,9 +26,10 @@ function App() {
         </ul>
       </div>
       <Routes>
+        <Route path="/home" element={<Home/>}/>
         <Route path="/about" element={<About />} />
         <Route path="/proyectos" element={<Projects />} />
-        <Route path="/contacto" element={<h1>Contacto</h1>} />
+        <Route path="/contacto" element={<Contact/>} />
       </Routes>
     </Router>
   );
