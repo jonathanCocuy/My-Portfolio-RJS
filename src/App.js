00367c70487fbc6c39068fc85111c9b072945bba
jonathanCocuy <jonathancp145@gmail.com>
2023-08-29 23:38:22 -0500

    Four-Step

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./Components/Main/Home/Home";
 import About from "./Components/Main/About/About";
 import Projects from "./Components/Main/Projects/Projects";
-import Home from "./Components/Main/Home/Home";
+import Experience from "./Components/Main/Experience/Experience";
 import Contact from "./Components/Main/Contact/Contact";
+import "./Images/Logo-JC.png"
 import "./App.css";
 
 /* Barra de navegación */
@@ -10,28 +13,45 @@ function App() {
   return (
     <Router>
       <div className="div-nav">
+      <Link hrefLang="./Images/Logo-JC.png"></Link>
         <ul className="navegation">
           <li className="list-nav">
-            <Link to="/home" className="items-nav">Home</Link>
+            <Link to="/home" className="items-nav">
+              Home
+            </Link>
           </li>
           <li className="list-nav">
-            <Link to="/about" className="items-nav">Acerca de mí</Link>
+            <Link to="/about" className="items-nav">
+              Acerca de mí
+            </Link>
           </li>
           <li className="list-nav">
-            <Link to="/proyectos" className="items-nav">Proyectos</Link>
+            <Link to="/proyectos" className="items-nav">
+              Proyectos
+            </Link>
           </li>
           <li className="list-nav">
-            <Link to="/contacto" className="items-nav">Contacto</Link>
+            <Link to="/experience" className="items-nav">
+              Experiencia
+            </Link>
+          </li>
+          <li className="list-nav">
+            <Link to="/contacto" className="items-nav">
+              Contacto
+            </Link>
           </li>
         </ul>
       </div>
-      <Routes>
-        <Route path="/home" element={<Home/>}/>
+
+      <Routes className="elements-routes">
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/proyectos" element={<Projects />} />
-        <Route path="/contacto" element={<Contact/>} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/contacto" element={<Contact />} />
       </Routes>
     </Router>
+
   );
 }
 
--- src/Components/Main/Home/Home.js
+++ src/Components/Main/Home/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 function Home() {
-    return (
-        <h1>Home</h1>
-    )
+  return (
+    <div>
+      <h1>Home</h1>
+      <p></p>
+    </div>
+  );
 }
 
 export default Home;
