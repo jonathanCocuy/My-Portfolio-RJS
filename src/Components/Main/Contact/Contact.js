dc6ecc0d0e87d28ce503ad8adc27c4eb01f99c90
jonathanCocuy <jonathancp145@gmail.com>
2023-09-21 23:04:41 -0500

    Changes-contact-form

--- src/Components/Main/Contact/Contact.js
+++ src/Components/Main/Contact/Contact.js
@@ -7,21 +7,24 @@ const Contact = () => {
     subject: "",
     body: "",
   };
-  const [formData, setFormData] = useState(InitialState);
 
-  const handleChange = (e) => {
+  const [formContact, setFormContact] = useState(InitialState);
+
+  const LoadData = (e) => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
+    setFormContact({
+      ...formContact,
       [name]: value,
     });
   };
 
   const SendEmail = (e) => {
     e.preventDefault();
-    const mailtoLink = `mailto:${formData.email}?subject=${encodeURIComponent(
-      formData.subject
-    )}&body=${encodeURIComponent(formData.body)}`;
+    const mailtoLink = `mailto:${
+      formContact.email
+    }?subject=${encodeURIComponent(
+      formContact.subject
+    )}&body=${encodeURIComponent(formContact.body)}`;
     window.location.href = mailtoLink;
   };
 
@@ -31,50 +34,54 @@ const Contact = () => {
         <h1>Puedes contactarte conmigo:</h1>
         <p className="contact-paragraph"></p>
       </div>
-      <form onSubmit={SendEmail} className="form-contact">
-        <div className="container-contact">
-          <div>
-            <input
-              className="input-contact"
-              type="text"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Email"
-              required
-            />
+      <div className="container-two-forms">
+        <form onSubmit={SendEmail} className="form-contact">
+          <div className="container-contact">
+            <div>
+              <input
+                className="input-contact"
+                type="text"
+                id="email"
+                name="email"
+                value={formContact.email}
+                onChange={LoadData}
+                placeholder="Email"
+                required
+                disabled
+              />
+            </div>
+            <div>
+              <input
+                className="input-contact"
+                type="text"
+                id="subject"
+                name="subject"
+                value={formContact.subject}
+                onChange={LoadData}
+                placeholder="Asunto"
+                required
+              />
+            </div>
+            <div>
+              <textarea
+                className="textarea-contact"
+                id="body"
+                name="body"
+                value={formContact.body}
+                onChange={LoadData}
+                placeholder="Mensaje"
+                required
+              />
+            </div>
           </div>
-          <div>
-            <input
-              className="input-contact"
-              type="text"
-              id="subject"
-              name="subject"
-              value={formData.subject}
-              onChange={handleChange}
-              placeholder="Asunto"
-              required
-            />
+          <div className="container-button-contact">
+            <button className="button-contact" type="submit">
+              Enviar
+            </button>
           </div>
-          <div>
-            <textarea
-              className="textarea-contact"
-              id="body"
-              name="body"
-              value={formData.body}
-              onChange={handleChange}
-              placeholder="Mensaje"
-              required
-            />
-          </div>
-        </div>
-        <div className="container-button-contact">
-          <button className="button-contact" type="submit">
-            Enviar
-          </button>
-        </div>
-      </form>
+        </form>
+        <div className="container-whatsapp">WhatsApp</div>
+      </div>
     </main>
   );
 };
