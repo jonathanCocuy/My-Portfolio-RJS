b4d6b4f1bd5e895c9428772038f7f4f960ebcc42
jonathanCocuy <jonathancp145@gmail.com>
2024-07-30 18:58:14 -0500

    Fixed render home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   NavLink,
+  Navigate,
   Outlet,
 } from "react-router-dom";
 import Home from "./Components/Main/Home/Home";
@@ -12,22 +13,23 @@ import Projects from "./Components/Main/Projects/Projects";
 import Experience from "./Components/Main/Experience/Experience";
 import Contact from "./Components/Main/Contact/Contact";
 import LogoImage from "./logo.png";
-import Footer from "./Components/Footer/Footer";
 import "./App.css";
 
 /* Barra de navegación */
 function App() {
   const [isNavOpen, setIsNavOpen] = useState(false);
+
   const SideBar = () => {
-    const nav = document.querySelector('.div-nav');
-    nav.classList.toggle('open');
-    setIsNavOpen(!isNavOpen); 
+    const nav = document.querySelector(".div-nav");
+    nav.classList.toggle("open");
+    setIsNavOpen(!isNavOpen);
   };
 
   const CloseAutoSB = () => {
-    const nav = document.querySelector('.div-nav');
-    nav.classList.remove('open');
-    setIsNavOpen(false); }
+    const nav = document.querySelector(".div-nav");
+    nav.classList.remove("open");
+    setIsNavOpen(false);
+  };
 
   return (
     <div>
@@ -36,7 +38,7 @@ function App() {
           &#9776;
         </button>
         <div className="div-nav">
-          <img className="img" src={LogoImage} alt="Logo Jonathan Cocuy"></img>
+          <img className="img" src={LogoImage} alt="Logo Jonathan Cocuy" />
           <ul className="navegation">
             <li className="list-nav">
               <NavLink to="/" className="items-nav" onClick={CloseAutoSB}>
@@ -65,7 +67,9 @@ function App() {
               <Outlet />
             </li>
           </ul>
-          <button className="close-button" onClick={SideBar}>&times;</button>
+          <button className="close-button" onClick={SideBar}>
+            &times;
+          </button>
         </div>
 
         <Routes className="elements-routes">
@@ -74,11 +78,9 @@ function App() {
           <Route path="/proyectos" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
           <Route path="/contacto" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/" />} /> {/* Redireccionar rutas no especificadas */}
         </Routes>
       </Router>
-      <div>
-        <Footer />
-      </div>
     </div>
   );
 }
--- src/Components/Footer/footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Footer () {
-    <div>Este es el footer</div>
-}
-
-export default Footer;
